b3e49b29637ff406f02fa0f18dd59476478cdd75
Natalia Cuenca <ncuenca@chicagobooth.edu>
2023-04-07 18:00:31 +0000

    Initial Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+def play_rock
+  #write code 
 
+  redirect_to("https://www.wikipedia.org/")
+end
 end

d0b5afe9ca056bf6197c22ede4cce11a432c2d79
Natalia Cuenca <ncuenca@chicagobooth.edu>
2023-04-10 02:14:47 +0000

    rps complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,13 @@ def homepage
   render({:template =>"game_templates/rules.html.erb"})
 end
 
+def play_paper
+  render({:template =>"game_templates/user_paper.html.erb"})   
+end
 
+def play_scissors
+  render({:template =>"game_templates/user_scissors.html.erb"})   
+end
 
 def play_rock
   #write code 
